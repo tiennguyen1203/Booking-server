594ca7729f68d4306bcb4c9886ac9eb257f469c6
TienNguyen <mac@Macs-MacBook-Air.local>
2021-02-11 20:43:14 +0700

    clean-code/remove-console-log

--- src/model/locations/location.repository.ts
+++ src/model/locations/location.repository.ts
@@ -6,7 +6,6 @@ import { Location } from './location.entity';
 @EntityRepository(Location)
 export class LocationRepository extends Repository<Location> {
   async getLocations(queryDto: QueryDto): Promise<[Location[], number]> {
-    console.log('adfadgsdfg', queryDto);
     const { take, skip } = queryDto;
     return this.findAndCount({ take, skip });
   }
