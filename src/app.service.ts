8d79d9ec78e0e55973f1f5ea3e280cec721a162c
TienNguyen <mac@Macs-MacBook-Air.local>
2021-02-15 17:41:52 +0700

    feat: API get app config GET /app/config

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BaseLocationTypeRepository } from './modules/base/location-types/location-type.repository';
+import { BaseLocationRepository } from './modules/base/locations/location.repository';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(BaseLocationTypeRepository)
+    private readonly baseLocationTypeRepository: BaseLocationTypeRepository,
+  ) {}
   getHello(): string {
     return 'Move to /docs to get APIs son of bitch!';
+  }
+
+  async getAppConfig(): Promise<any> {
+    const locationTypes = await this.baseLocationTypeRepository.find();
+    return {
+      locationTypes,
+    };
   }
 }
