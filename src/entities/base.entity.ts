7701186e0028d173f3ffef856c0bdb86f5565dd0
TienNguyen <tien.nguyen@team.enouvo.com>
2021-04-03 15:16:25 +0700

    fix: Fix params id number -> uuid

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -1,20 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
+  BaseEntity,
+  Column,
   CreateDateColumn,
+  PrimaryColumn,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
-  BaseEntity,
 } from 'typeorm';
 
 export abstract class CustomBaseEntity extends BaseEntity {
+  @Column({ type: 'uuid' })
   @PrimaryGeneratedColumn('uuid')
-  @ApiProperty()
+  @ApiProperty({ type: 'uuid' })
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
   @ApiProperty()
   createdAt?: Date;
 
+  @Column()
   @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
   @ApiProperty()
   updatedAt?: Date;
