1c9a57b76fd69c7b0eba2803b5f619bf05a37bb9
TienNguyen <tien.nguyen@team.enouvo.com>
2021-04-03 16:19:03 +0700

    feat: City

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,21 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BaseLocationTypeRepository } from './modules/base/location-types/location-type.repository';
 import { BaseLocationRepository } from './modules/base/locations/location.repository';
+import { BaseCityRepository } from './modules/base/cities/city.repository';
 
 @Injectable()
 export class AppService {
   constructor(
     @InjectRepository(BaseLocationTypeRepository)
     private readonly baseLocationTypeRepository: BaseLocationTypeRepository,
+
+    @InjectRepository(BaseCityRepository)
+    private readonly baseCityRepository: BaseCityRepository,
   ) {}
   getHello(): string {
     return 'Move to /docs to get APIs son of bitch!';
   }
 
   async getAppConfig(): Promise<any> {
-    const locationTypes = await this.baseLocationTypeRepository.find();
+    const [locationTypes, cities] = await Promise.all([
+      this.baseLocationTypeRepository.find(),
+      this.baseCityRepository.find(),
+    ]);
     return {
       locationTypes,
+      cities,
     };
   }
 }
