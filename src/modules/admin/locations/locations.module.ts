6123a0641fc0f1f2fde5fe8daf195ca506835f4d
TienNguyen <tien.nguyen@team.enouvo.com>
2021-05-12 13:54:29 +0700

    fix: Fix passport

--- src/modules/admin/locations/locations.module.ts
+++ src/modules/admin/locations/locations.module.ts
@@ -3,9 +3,13 @@ import { Module } from '@nestjs/common';
 import { AdminLocationsController } from './locations.controller';
 import { AdminLocationsService } from './locations.service';
 import { AdminLocationRepository } from './location.repository';
+import { passportModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AdminLocationRepository])],
+  imports: [
+    TypeOrmModule.forFeature([AdminLocationRepository]),
+    passportModule,
+  ],
   controllers: [AdminLocationsController],
   providers: [AdminLocationsService],
 })
